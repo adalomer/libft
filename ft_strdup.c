84534312a785602c9754de7c18c0808a1b5f8703
Omer Adali <adalomer60@gmail.com>
2024-09-28 12:42:30 +0300

    fifth

--- ft_strdup.c
+++ ft_strdup.c
@@ -1,41 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: omadali <adalomer60@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/27 14:34:02 by omadali           #+#    #+#             */
-/*   Updated: 2024/09/28 12:42:10 by omadali          ###   ########.fr       */
+/*   Created: 2024/09/28 02:08:15 by omadali           #+#    #+#             */
+/*   Updated: 2024/09/28 12:03:30 by omadali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void *ft_memmove(void *dest, const void *src, int n)
+#include <stdlib.h>
+char *ft_strdup(const char *s)
 {
-	if (!dest || !src)
-		return (0);
-	char *d;
-	char *s;
-	s = (char *)src;
-	d = (char *)dest;
-	if (d - s > 0 )
+	int b;
+	char *a;
+	b = 0;
+	while(s[b] != '\0')
+	b++;
+	a = (char *)malloc(b*sizeof(char)+ 1);
+	b = 0;
+	while(s[b] != '\0')
 	{
-		d += n - 1;
-		s += n - 1;
-		while(n > 0)
-		{
-			*d-- = *s--;
-			n--;
-		}
+		a[b] = s[b];
+		b++;
 	}
-	else 
-	{
-		while(0 < n)
-		{
-		*d++ = *s++;
-		n--;
-		}
-	}
-	return(dest);
-
+	a[b] = '\0';
+	return(a);
 }
