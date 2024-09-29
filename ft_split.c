0418fa701ccfc6df68f26aec1db89d9b116596a3
Omer Adali <adalomer60@gmail.com>
2024-09-29 21:03:06 +0300

    ten

--- ft_split.c
+++ ft_split.c
@@ -6,9 +6,63 @@
 /*   By: omadali <adalomer60@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/29 13:11:07 by omadali           #+#    #+#             */
-/*   Updated: 2024/09/29 14:27:57 by omadali          ###   ########.fr       */
+/*   Updated: 2024/09/29 20:23:54 by omadali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft.h"
+int word_count(char *s, char c)
+{
+int a;
+    int b;
+    a = 0;
+    b = 0;
+    while(s[a] != '\0')
+    {
+      while(s[a] == c && s[a] != '\0')
+      a++; 
+      if(s[a] != c && s[a] != '\0')
+      b++;
+      while(s[a] != c && s[a] != '\0')
+      a++;
+    }
+    return (b);
+}
+int word_len(char *s,char c)
+{
+	int a;
+	a = 0;
+    while (s[a] && s[a] != c)
+        a++;
+    return (a);
+	
+}
+char **ft_split(char const *s, char c)
+{
+	int i;
+	int b;
+	int l;
+	int k;
+	char **f;
+	k = 0;
+	i = 0;
+	l = 0;
+	b = 0;
+	**f = malloc(sizeof(char)*word_count(s,c)+1);
+	while(s[i] != '\0')
+	{
+		if(s[i] != c)
+		{
+			b = word_len(&s[i], c);
+            f[l] = malloc(b + 1);
+			while (s[i] && s[i] != c)
+        	f[k][b++] = s[i++];
+            f[k++][b] = '\0';
+        } else
+            i++;
+		}
+		f[k] = NULL;
+    return (f);
+	}
+}
