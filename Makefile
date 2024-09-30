c0731c715a762ab7df399b96a591f8877b28652a
Omer Adali <adalomer60@gmail.com>
2024-09-30 22:06:23 +0300

    thirsty

--- Makefile
+++ Makefile
@@ -6,8 +6,51 @@
 #    By: omadali <adalomer60@gmail.com>             +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/09/27 13:45:19 by omadali           #+#    #+#              #
-#    Updated: 2024/09/30 17:14:50 by omadali          ###   ########.fr        #
+#    Updated: 2024/09/30 22:05:21 by omadali          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-NAME
+NAME = lib		ft.a
+
+CC = gcc
+
+SRCS =	ft_atoi.c \
+		ft_bzero.c \
+		ft_calloc.c \
+		ft_isalnum.c \
+		ft_isalpha.c \
+		ft_isascii.c \
+		ft_isdigit.c \
+		ft_isprint.c \
+		ft_itoa.c \
+		ft_memchr.c \
+		ft_memcmp.c \
+		ft_memcpy.c \
+		ft_memmove.c \
+		ft_memset.c \
+		ft_putchar_fd.c \
+		ft_putendl_fd.c \
+		ft_putnbr_fd.c \
+		ft_putstr_fd.c \
+		ft_split.c \
+		ft_strchr.c \
+		ft_strdup.c \
+		ft_striteri.c \
+		ft_strjoin.c \
+		ft_strlcat.c \
+		ft_strlcpy.c \
+		ft_strlen.c \
+		ft_strmapi.c \
+		ft_strncmp.c \
+		ft_strnstr.c \
+		ft_strrchr.c \
+		ft_strtrim.c \
+		ft_substr.c \
+		ft_tolower.c \
+		ft_toupper.c
+OBJS=$(SRCS:.c=.o)
+
+zort:
+	@echo $(OBJS)
+
+		
--- libft.h
+++ libft.h
@@ -1,31 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omadali <adalomer60@gmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/30 21:47:13 by omadali           #+#    #+#             */
+/*   Updated: 2024/09/30 21:49:43 by omadali          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef LIBFT_H
-#define LIBFT_H
+# define LIBFT_H
 
-#include <stdio.h>
-#include <unistd.h>
-#include <ctype.h>
-
-
-int ft_atoi(char *a);
-int ft_strlen(char *c);
-int ft_isalnum(char d);
-int ft_isalpha(char c);
-int ft_isascii(char e);
-int ft_isdigit(char e);
-int ft_isprint(int c);
-void ft_bzero(void *s, int n);
+int		ft_atoi(char *a);
+int		ft_strlen(char *c);
+int		ft_isalnum(char d);
+int		ft_isalpha(char c);
+int		ft_isascii(char e);
+int		ft_isdigit(char e);
+int		ft_isprint(int c);
+void	ft_bzero(void *s, int n);
 void	ft_calloc(int b, int size);
-void *ft_memcpy(void *dest, const void *src, int n);
-void *ft_memmove(void *dest, const void *src, int n);
-char *ft_strjoin(char const *s1, char const *s2);
-char *ft_substr(char const *s, unsigned int start,int len);
-char *ft_strtrim(char const *s1, char const *set);
-int ft_strlcpy(char *dst, const char *src, int size);
-char **ft_split(char const *s, char c);
-char *ft_strmapi(char const *s, char (*f)(unsigned int, char));
-void ft_putstr_fd(char *c,int fd);
-void ft_putchar_fd(char c, int fd);
-void ft_putendl_fd(char *s, int fd);
-void ft_putnbr_fd(int n, int fd);
+void	*ft_memcpy(void *dest, const void *src, int n);
+void	*ft_memmove(void *dest, const void *src, int n);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_substr(char const *s, unsigned int start, int len);
+char	*ft_strtrim(char const *s1, char const *set);
+int		ft_strlcpy(char *dst, const char *src, int size);
+char	**ft_split(char const *s, char c);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_putstr_fd(char *c, int fd);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 
 #endif
